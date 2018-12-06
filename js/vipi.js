96b9d151e2ccaf0234f4f2fa6d1800d4c46fbe4d
Nero <nero98@sfr.fr>
2018-12-06 23:11:15 +0100

    add basic game loop

---
+++ js/vipi.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    execute()
+});
+
+function execute(){
+
+    var header_height_str = $("#header").css("height");
+    var header_width_str = $("#header").css("width");
+    $("#canvas").css("height", header_height_str);
+    $("#canvas").css("width", header_width_str);
+
+    var header_height = header_height_str.replace("px", "");
+    var header_width = header_width_str.replace("px", "");
+    var canvas=document.getElementById("canvas");
+    canvas.height = header_height;
+    canvas.width = header_width;
+
+    var context = canvas.getContext("2d");
+    var width = canvas.width;
+    var height = canvas.height;
+
+    var entities = [];
+    // TODO: init all the stuff
+
+    requestAnimationFrame(update);
+}
+
+function update(){
+
+}
